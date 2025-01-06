69e0c42ede2f3d086592e0c65a75c65aa290eef5
Ahmad Raza <ahmad.raza@geekybugs.com>
2025-01-06 12:49:23 +0500

    UI Improvement

--- src/pages/ListenQuran.jsx
+++ src/pages/ListenQuran.jsx
@@ -82,15 +82,15 @@ const ListenQuran = () => {
       if (chapter !== -1 && reciter !== -1) {
           fecthRecitation();
       }
-  }, [chapter]);
+  }, [chapter, reciter]);
 
     return (
         <div className='justify-center flex flex-col items-center'>
-               <div className='flex flex-row gap-4 items-center w-11/12 md:w-4/6 lg:w-2/4'>
+               <div className='flex flex-row  gap-4 items-center w-11/12 md:w-4/6 lg:w-2/4'>
                <select value={reciter} onChange={(e)=>{setReciter(e.target.value)
                 setShow(false)
                 setShow1(false)
-               }}  defaultValue={-1} className='bg-green-400 px-2 py-2 rounded-xl text-white justify-center flex mb-3 w-2/4'>
+               }}  defaultValue={-1} className='bg-green-400 focus:!outline-none px-2 py-2 rounded-xl text-white justify-center flex mb-3 w-2/4'>
                 <option value={-1}>Select Reciter</option>
                 {reciters?.map((reciter)=>(
                     <option key={reciter.id} value={reciter.id}>{reciter.reciter_name} {reciter.style?`(${reciter.style})`:''}</option>
@@ -100,7 +100,7 @@ const ListenQuran = () => {
                 // setShow(false)
                 setShow1(false)
                 }
-                } defaultValue={-1} className='bg-green-400 px-2 py-2 rounded-xl text-white justify-center flex mb-3 w-2/4'>
+                } defaultValue={-1} className='bg-green-400 px-2 py-2 rounded-xl focus:!outline-none text-white justify-center flex mb-3 w-2/4'>
                 <option value={-1}>Select Surah</option>
                 {chapters?.map((chapter)=>(
                     <option key={chapter.id} value={chapter.id}>{chapter.name_simple}</option>
@@ -108,27 +108,25 @@ const ListenQuran = () => {
                 </select>
                </div>
             
-                            <button disabled={reciter===-1 || chapter===-1} onClick={fecthRecitation} className='bg-green-400 px-4 py-2 rounded-full text-white justify-center flex mb-3'> 
-                                Find Recitation
-                            </button>
+                           
                             {show1 &&
                             <div className='flex flex-col items-center bg-green-500 p-4 rounded-xl text-white w-11/12 md:w-4/6 lg:w-2/4'>
-                                <h1 className='mb-3'>{chapters[chapter-1]?.name_simple}</h1>
+                                <h1 className='mb-3 font-semibold text-lg'>{chapters[chapter-1]?.name_simple} by: {reciters[reciter-1]?.reciter_name}</h1>
                             {show &&
                                 <audio controls className='w-11/12'>
                             <source src={recitation.audio_url} type="audio/mpeg"/>
                             </audio>
                             }
                             <div className='flex flex-row justify-around mt-4 w-full'>
-                                <button disabled={chapter===1} onClick={()=>{
+                                <button disabled={1===chapter} onClick={()=>{
                                     setChapter((prev) => prev - 1);
                                     setShow(false)
-                                }} className='bg-white text-green-700 p-1 w-2/5 rounded-full'>Previous</button>
-                                <button disabled={chapter===114} onClick={()=>{
+                                }} className='bg-white text-green-700 p-1 w-2/5 text-sm md:text-lg rounded-full'>Previous Surah</button>
+                                <button disabled={114===chapter} onClick={()=>{
                                     setChapter((prev) => Number(prev) + 1);
                                     console.log(chapter)
                                     setShow(false)
-                                }} className='bg-white text-green-700 p-1 w-2/5 rounded-full'>Next</button>
+                                }} className='bg-white text-green-700 p-1 w-2/5 text-sm md:text-lg rounded-full'>Next Surah</button>
                                  </div>
                             </div>}
         </div>
